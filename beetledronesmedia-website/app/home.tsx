d2ad35ee449a2da25b75e89271bd85279eed9407
Everett Merrill <artofewm@gmail.com>
2026-04-21 21:52:47 -0700

    Layout: sections and use client

---
+++ beetledronesmedia-website/app/home.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React, { useState, useEffect, useRef } from "react";
+import "./globals.css";
+import Lenis from 'lenis';
+
+
+
+
+function Home() {
+    
+
+// useEffects
+
+// Touch Events for Mobile
+
+//
+
+}
