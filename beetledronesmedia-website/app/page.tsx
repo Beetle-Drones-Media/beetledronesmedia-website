b4c7ef414a8206f034e8021855612b5a9497cd13
Everett Merrill <artofewm@gmail.com>
2026-04-22 16:21:15 -0700

    "Staging all commits"

--- beetledronesmedia-website/app/page.tsx
+++ beetledronesmedia-website/app/page.tsx
@@ -1,37 +1,28 @@
 "use client"
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+
+
+
 
 export default function Page() {
   return (
     <>
     <main>
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <div>
-      // hero video - section 1
-     </div>
-     <div>
-      // about us - section 2
-     </div>
-     <div>
-      // portfolio - section 3
-     </div>
-     <div>
-      // contact us - section 4
-     </div>
-     <div>
-      // copyright - section 5
-        <footer className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+     <HeroSection id="hero" />
+     <AboutSection id="about" />
+     {/* <PortfolioSection id="portfolio" />
+     <ContactSection id="contact" /> */}
+     <footer className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
           <p>
             ©2026 Beetle Drones Media. <br />
             All rights reserved. <br />
             Privacy Policy | Contact
           </p>
         </footer>
-
      </div>
-    </div>
-
     </main>
-   
     </>
   );
 }
