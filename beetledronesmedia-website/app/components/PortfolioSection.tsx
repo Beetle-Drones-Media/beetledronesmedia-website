319edcf96bd15ef75a48eaf9bbd92f7dd9cb096b
Everett Merrill <artofewm@gmail.com>
2026-04-27 21:36:02 -0700

    Chores: Superficial things and portfolio upgrade

--- beetledronesmedia-website/app/components/PortfolioSection.tsx
+++ beetledronesmedia-website/app/components/PortfolioSection.tsx
@@ -2,6 +2,10 @@
 import { useState, useEffect } from "react";
 
 
+function Header({ children }) {
+  return <h2 className="text-6xl font-bold mb-4 text-green-400 text-center">{children}</h2>;
+}
+
 function PortfolioSection(){
 
     /**
@@ -24,38 +28,26 @@ function PortfolioSection(){
     const file = portfolio[current];
     const isVideo = file.endsWith(".webm") || file.endsWith(".mp4");
     return (
-        <>
-        <div className="relative w-full h-[100vh] min-h-[400px] overflow-hidden bg-black">
-
         
-        <section id="portfolio">
-           
-         <main 
-         
-          className="relative">
-            {isVideo ? (
-            <video
-            className="backgroundImage"
-            autoPlay
-            muted
-            loop
-            key={file}
-          >
-            <source src={ `/${file}` } type="video/webm" />
-            </video>
-            ) : (
-        <img
-          className="backgroundImage"
-          src={`/${file}`}
-          alt="Portfolio"
-          key={file}
-        />
-            )}
-        </main>
-         
-        </section>
-        </div>
-        </>
+      <div className="relative w-full h-[100vh] overflow-hidden bg-black">
+    <section id="portfolio">
+        <Header>Drone Portfolio</Header>
+      <main className="relative">
+        {isVideo ? (
+          <video className="backgroundImage" autoPlay muted loop key={file}>
+            <source src={`/${file}`} type="video/webm" />
+          </video>
+        ) : (
+          <img className="backgroundImage" src={`/${file}`} alt="Portfolio" key={file} />
+        )}
+      </main>
+    </section>
+    <img
+      src="/BDMPortfolioContainer.png"
+      className="absolute top-0 left-0 w-full h-full object-cover z-10 pointer-events-none"
+      alt=""
+    />
+  </div>
     );
 }
 
